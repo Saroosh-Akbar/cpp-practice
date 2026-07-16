d2dbcc260899775d781083c511c8607cfcde7b74
Saroosh Akbar <muhammadsaroosh296@gmail.com>
2026-07-16 06:47:30 -0700

    Added variables program

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age=20;
+    float height=5.7;
+    double pi=3.141592;
+    char grade='A';
+    bool isStudent=true;
+    string name="Saroosh";
+cout<<"Name:"<<name<<endl;
+cout<<"Age:"<<age<<endl;
+cout<<"Height:"<<height<<endl;
+cout<<"Pi:"<<pi<<endl;
+cout<<"Grade:"<<grade<<endl;
+cout<<"Student:"<<isStudent<<endl;
+return 0;
+}

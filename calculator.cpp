b31e5aedbd3dd5b375047a3018f39dc1b0580b20
Saroosh Akbar <muhammadsaroosh296@gmail.com>
2026-07-16 07:06:27 -0700

    Added calculator program

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+cout<<"Welcome to Saroosh's Calculaltor"<<endl;
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Addition = " << num1 + num2 << endl;
+    cout << "Subtraction = " << num1 - num2 << endl;
+    cout << "Multiplication = " << num1 * num2 << endl;
+    cout << "Division = " << num1 / num2 << endl;
+    cout << "Remainder = " << num1 % num2 << endl;
+    cout<<"Got a good experience"<<endl;
+
+    return 0;
+}

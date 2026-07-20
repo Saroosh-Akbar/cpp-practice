d65a2df484244e0b7348288908a5bb306ca14f34
Saroosh Akbar <muhammadsaroosh296@gmail.com>
2026-07-20 07:09:55 -0700

    Add sum of n program

---
+++ sum_of_n.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    int sum=0;
+    cout<<"Enter a number";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+            cout<<"Sum="<<sum<<endl;
+
+    return 0;
+}
